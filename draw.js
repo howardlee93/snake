084fdde427ad73195e9adb4955b2bee1af994987
howard lee <howardlee@howards-MacBook-Pro.local>
2022-01-25 14:09:13 -0800

    init

---
+++ draw.js
@@ -0,0 +1,19 @@
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+
+
+const DrawCanvas =( width, height,) =>{
+    this.width = width;
+    this.height = height;
+
+
+    
+}
+
+
+const DrawField;
+
+
+const DrawSnake;
---
+++ snake.js
@@ -0,0 +1,30 @@
+//snake.js
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const scale = 10;
+
+
+const DrawCanvas =( width, height,) =>{
+    this.width = width;
+    this.height = height;
+
+
+    
+}
+
+const DrawField;
+
+
+const DrawSnake;
+
+const Fruit;
+
+const Move;
+
+const main;
+
+
+
+
+main();
